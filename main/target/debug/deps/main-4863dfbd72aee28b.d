8f73f381b4243c3aafb806c55401aa988f5fa81e
harrrrion <harrisonrigby@icloud.com>
2024-03-28 23:02:51 +0000

    Error message now outputted

---
+++ main/target/debug/deps/main-4863dfbd72aee28b.d
@@ -0,0 +1,5 @@
+/Users/harrisonrigby/Desktop/UniCS/FirstYear/rustLearning/web_server/main/target/debug/deps/libmain-4863dfbd72aee28b.rmeta: src/main.rs
+
+/Users/harrisonrigby/Desktop/UniCS/FirstYear/rustLearning/web_server/main/target/debug/deps/main-4863dfbd72aee28b.d: src/main.rs
+
+src/main.rs:
---
+++ main/target/debug/deps/main-f5f7a73c661d64d6.d
@@ -0,0 +1,5 @@
+/Users/harrisonrigby/Desktop/UniCS/FirstYear/rustLearning/web_server/main/target/debug/deps/main-f5f7a73c661d64d6: src/main.rs
+
+/Users/harrisonrigby/Desktop/UniCS/FirstYear/rustLearning/web_server/main/target/debug/deps/main-f5f7a73c661d64d6.d: src/main.rs
+
+src/main.rs:
