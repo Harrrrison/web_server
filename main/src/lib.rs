818388d31282500de7c82754246bdab897f32432
harrrrion <harrisonrigby@icloud.com>
2024-03-31 20:08:53 +0100

    now shuts down gracefully

--- main/src/lib.rs
+++ main/src/lib.rs
@@ -5,12 +5,12 @@ use std::{
 
 pub struct ThreadPool {
     workers: Vec<Worker>,
-    sender: mpsc::Sender<Job>,
+    sender: Option<mpsc::Sender<Job>>,
 }
 
 pub struct Worker {
     id: usize,
-    thread: thread::JoinHandle<()>,
+    thread: Option<thread::JoinHandle<()>>,
 }
 
 type Job = Box<dyn FnOnce() + Send + 'static>;
@@ -22,7 +22,7 @@ impl ThreadPool {
     {
         let job = Box::new(f);
 
-        self.sender.send(job).unwrap();
+        self.sender.as_ref().unwrap().send(job).unwrap();
     }
 
     pub fn new(size: usize) -> ThreadPool {
@@ -37,23 +37,52 @@ impl ThreadPool {
         for id in 0..size {
             workers.push(Worker::new(id, Arc::clone(&receiver)));
         }
-        ThreadPool { workers, sender }
+        ThreadPool {
+            workers,
+            sender: Some(sender),
+        }
     }
 }
 
 impl Worker {
     fn new(id: usize, receiver: Arc<Mutex<mpsc::Receiver<Job>>>) -> Worker {
         let thread = thread::spawn(move || loop {
-            let job = receiver.lock().unwrap().recv().unwrap();
+            //let job = receiver.lock().unwrap().recv().unwrap();
             // lock is called to acquire the mutex and then we call unwrap to panic on errors
-            //
-            println!("Worker {id} got a job; executing now.");
+            let message = receiver.lock().unwrap().recv();
 
-            job();
+            match message {
+                Ok(job) => {
+                    println!("Worker {id} got a job; executing now.");
+                    job();
+                }
+                Err(_) => {
+                    println!("Worker {id} disconnected; shutting down");
+                    break;
+                }
+            }
         });
         //NOTE if the os cannot create any mroe threads
         //this will panic we'd want to use thead::Builder and its spawn method that retus Result
 
-        Worker { id, thread }
+        Worker {
+            id,
+            thread: Some(thread),
+        }
+    }
+}
+
+impl Drop for ThreadPool {
+    fn drop(&mut self) {
+        drop(self.sender.take()); // we drop sende to close the channel to assert that no more
+                                  // messages can be sent
+        for worker in &mut self.workers {
+            println!("Shutting down worker {}", worker.id);
+
+            if let Some(thread) = worker.thread.take() {
+                thread.join().unwrap(); // as we cant directly the thread from join worker we need to take the
+                                        // thread out of worker then join it from there
+            }
+        }
     }
 }
